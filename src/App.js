b5b31d69aa5ac64440fa7ac9bc0785de95674546
CJ Olsen <cjols97@iastate.edu>
2022-11-21 17:22:36 -0600

    Add timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './Components/Timer/Timer.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header>
+      <Timer />
     </div>
   );
 }
