0f70d3837bf52e0720f7a24bdbaa111c5752d2a3
CJ <cj.olsen16@gmail.com>
2024-05-28 11:43:44 -0500

    Create Record component

---
+++ src/Components/Record/Record.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface RecordProps {
+    recordedTimes: number[]
+}
+
+const Record: React.FC<RecordProps> = ({ recordedTimes }) => {
+    return (
+        <div>
+            <h2>Recorded Times:</h2>
+            <ul>
+                {recordedTimes.map((recordedTime, index) => (
+                    <li key={index}>{recordedTime}s</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Record
